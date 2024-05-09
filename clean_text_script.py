32e7c0d073670451689f5f6cebef140c09613ca4
Giuseppe Muschetta <peppe212@gmail.com>
2024-05-09 22:36:49 +0200

    Add files via upload

--- clean_text_script.py
+++ clean_text_script.py
@@ -1,3 +1,5 @@
+# Author: Giuseppe Muschetta
+
 from bs4 import BeautifulSoup
 import re
 import spacy
--- count_and_wordclouds.py
+++ count_and_wordclouds.py
@@ -1,3 +1,5 @@
+# Author: Giuseppe Muschetta
+
 import pandas as pd
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
